698e5f04dd835023132c372e122bd22ef4c55671
2452269 Huynh Tri Duc <duc.huynh95@hcmut.edu.vn>
2026-09-18 10:02:26 +0700

    Add files via upload

--- Ex8/main.c
+++ Ex8/main.c
@@ -107,17 +107,14 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-	  for (int i = 0; i < 12; i++) {
-		  clearAllClock();
-		  setNumberOnClock(i);
-	      HAL_Delay(1000);
-	  }
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
+while (1)
+{
+  for (int i = 0; i < 12; i++) {
+	  clearAllClock();
+	  setNumberOnClock(i);
+	  HAL_Delay(1000);
   }
+}
   /* USER CODE END 3 */
 }
 
